be6017929f09295a12dd94bb610c35766b701313
angelika22 <70664491+angelika22@users.noreply.github.com>
2020-11-11 10:16:31 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+n=eval(input("n= "))
+if n in (28, 29, 30, 31):
+    if n==28:
+        print("februie")
+    elif n==29:
+        print("februrie, an bisect")
+    elif n==30:
+        print("aprilie, iunie, septembrie, noiembrie")
+    elif n==31:
+        print("ianuarie, martie, mai, iulie, august, octombrie, decembrie")
+else:
+    print("de introdus numerele 28, 29, 30, 31")
